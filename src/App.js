b0ffb1b1b20537f00d34c8c7198092a7d2f74a8e
imisrafil <im.israfil.fi@gmail.com>
2023-06-13 17:36:56 +0600

    Learn to use react useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from "react";
+const App = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(0);
 
-function App() {
+  const [num1] = useState(5);
+  const [num2] = useState(9);
+
+  const sum = useCallback(() => num1 + num2, [num1, num2]);
+
+  const build = useCallback(() => [num1, num2], [num1, num2]);
+
+  useEffect(() => {
+    // let isMounted = true;
+    // if (isMounted) {
+    //   console.log("run useEffect");
+    // }
+    // return () => (isMounted = false);
+
+    console.log(`value: ${build()}`);
+    setResult(build());
+  }, [build]);
+
+  console.log("The component redered");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
     </div>
   );
-}
+};
 
 export default App;
